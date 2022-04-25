7ec7f5ae7c530f9cef7f0cc9980dcda724cf33f2
Minho Ji <59356172+minho4418@users.noreply.github.com>
2022-04-25 23:28:50 +0900

    Update 20220421_index.js

--- basic/20220421_index.js
+++ basic/20220421_index.js
@@ -9,3 +9,15 @@ app.listen(8082, function(){
 app.get('/', function(req,res){
     res.send("안녕하세요!!!")
 });
+
+
+// http로 서버띄우기
+//const http = require("http");
+//const app = http.creatServer((req,res) => {
+    //res.writeHead(200, {"Content-Type": "text/html; charset=utf-t"});
+    //if (req.url === "/") {
+        //res.end("여기는 루트 입니다.");
+    //} else if(req.url === "/login"){
+        //res.end("여기는 로그인 화면입니다.");
+    //}
+//});
